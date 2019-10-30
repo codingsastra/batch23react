be2eb5315681e5de0bc775594ad0779b1112c70d
Varma Bhupatiraju <varmab@gmail.com>
2019-10-30 19:27:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Welcome from './Welcome'
+import Counter from './Counter'
+
+class  App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Welcome name="Ram" year={2020}/>
+          <Welcome name="Varma"/>
+        </header>
+        <Counter/>
+      </div>
+    );
+  }
 }
 
 export default App;
